60f7ff5829553296ef6372a182443fff40a3d035
Kelvin <64967043+kelvinjang@users.noreply.github.com>
2020-07-18 03:17:22 +0900

    Add files via upload

---
+++ P6/hello.py
@@ -0,0 +1,8 @@
+from cs50 import get_string
+
+# Customized "hello, world" program
+
+# Gets name of user
+answer = get_string("What is your name?\n")
+# Prints customized "hello, world"
+print("hello, " + answer)
