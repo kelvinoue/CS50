a6373122f90663a5f2f9650e6f1c42b5529cde2e
Kelvin <64967043+kelvinjang@users.noreply.github.com>
2020-07-20 23:52:06 +0900

    Add files via upload

--- P7/7.sql
+++ P7/7.sql
@@ -1,3 +1,2 @@
-SELECT year, title, rating FROM movies JOIN ratings ON movies.id = ratings.movie_id WHERE year = 2010 ORDER BY rating DESC, title
-
-/* Todo: Check for movies with no ratings */
+SELECT year, title, rating FROM movies
+JOIN ratings ON movies.id = ratings.movie_id WHERE year = 2010 AND rating != "\N" ORDER BY rating DESC, title
