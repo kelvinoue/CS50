7da1a4c8dd14479e5f376eb89f9d880ade1e9b60
Kelvin <64967043+kelvinjang@users.noreply.github.com>
2020-07-20 23:53:53 +0900

    Add files via upload

--- P7/9.sql
+++ P7/9.sql
@@ -1,3 +1,3 @@
-SELECT DISTINCT name, birth FROM people WHERE id IN
+SELECT DISTINCT name FROM people WHERE id IN
 (SELECT person_id FROM stars WHERE movie_id IN
 (SELECT id FROM movies WHERE year = 2004)) ORDER by birth
